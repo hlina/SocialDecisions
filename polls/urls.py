b5c7a6972f4f20282692a3d6ebc69d7329e1247f
Huang <lina.huang@oliverwyman.com>
2016-12-13 18:24:25 -0800

    finished mvp

--- polls/urls.py
+++ polls/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /polls/about/
     url(r'^about/$', views.about, name='about'),
-    # ex: /polls/adsfafd5/
-    url(r'^(?P<question_url>.{11}[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_url>.{11}[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<question_url>.{11}[0-9]+)/vote/$', views.vote, name='vote'),
+    # ex: /polls/adsfafd41f5/
+    url(r'^(?P<question_url>.{10}[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/adsfafd41f5/results/
+    url(r'^(?P<question_url>.{10}[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/adsfafd41f5/vote/
+    url(r'^(?P<question_url>.{10}[0-9]+)/vote/$', views.vote, name='vote'),
 ]
